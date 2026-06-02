752ab8f60978f3b6ba51094f82db8b3703aef1cd
wondiradbeziemitiku-max <wondiradbeziemitiku@gmail.com>
2026-06-02 14:06:10 +0300

    Create LoadingDialog.kt

---
+++ app/src/main/java/com/bingoapp/ui/components/LoadingDialog.kt
@@ -0,0 +1,20 @@
+package com.bingoapp.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingDialog() {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.size(80.dp)) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(48.dp)
+        )
+    }
+}
