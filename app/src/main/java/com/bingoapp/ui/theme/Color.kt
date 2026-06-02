d25178e524431c720811580c21f8823318e956c9
wondiradbeziemitiku-max <wondiradbeziemitiku@gmail.com>
2026-06-02 09:03:51 +0300

    Create Color.kt

---
+++ app/src/main/java/com/bingoapp/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.bingoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DeepBlue = Color(0xFF1A1A2E)
+val DarkBlue = Color(0xFF16213E)
+val MediumBlue = Color(0xFF0F3460)
+val Gold = Color(0xFFFFD700)
+val LightGold = Color(0xFFFFED4A)
+val BingoGreen = Color(0xFF22C55E)
+val DarkGreen = Color(0xFF16A34A)
+val BingoRed = Color(0xFFEF4444)
+val DarkRed = Color(0xFFDC2626)
+val SurfaceDark = Color(0xFF1E1E2E)
+val SurfaceVariantDark = Color(0xFF2A2A3E)
+val OnSurfaceDark = Color(0xFFE0E0E0)
+val OnSurfaceVariantDark = Color(0xFFAAAAAA)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray800 = Color(0xFF333333)
+val Gray600 = Color(0xFF666666)
+val Gray400 = Color(0xFF999999)
